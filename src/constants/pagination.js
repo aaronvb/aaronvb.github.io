bb02b3cadd7a7e91fb0bfc6d96012d1621501b1d
Aaron Van Bokhoven <bokhoven@gmail.com>
2020-04-26 23:07:59 -1000

    Change pagination text

--- src/constants/pagination.js
+++ src/constants/pagination.js
@@ -1,7 +1,7 @@
 // @flow strict
 const PAGINATION = {
-  PREV_PAGE: '← PREV',
-  NEXT_PAGE: '→ NEXT'
+  PREV_PAGE: '← Newer articles',
+  NEXT_PAGE: '→ Older articles'
 };
 
 export default PAGINATION;
