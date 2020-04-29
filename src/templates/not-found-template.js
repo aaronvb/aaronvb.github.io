5942f9e476b212e643a6a881e3dbd856b89c3703
Aaron Van Bokhoven <bokhoven@gmail.com>
2020-04-28 20:02:17 -1000

    Update 404 page

--- src/templates/not-found-template.js
+++ src/templates/not-found-template.js
@@ -12,7 +12,8 @@ const NotFoundTemplate = () => {
     <Layout title={`Not Found - ${title}`} description={subtitle}>
       <Sidebar />
       <Page title="NOT FOUND">
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>Sorry, the content you are trying to find is not here.</p>
+        <p>If you feel like there should be something here, please <a href="/contact">contact me</a>.</p>
       </Page>
     </Layout>
   );
