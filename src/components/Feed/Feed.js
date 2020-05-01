b5df68912092c1da5a8484d9359e751a88ed5aad
Aaron Van Bokhoven <bokhoven@gmail.com>
2020-04-30 22:06:30 -1000

    Adjust feed styling and make description a link

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -25,7 +25,9 @@ const Feed = ({ edges }: Props) => (
         <h2 className={styles['feed__item-title']}>
           <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
         </h2>
-        <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
+        <Link className={styles['feed__item-description-link']} to={edge.node.fields.slug}>
+          <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
+        </Link>
       </div>
     ))}
   </div>
