0fa638e31b067bd10afe88a6c7d1b02b1f13f5c4
Aaron Van Bokhoven <bokhoven@gmail.com>
2020-06-03 23:18:38 -1000

    Move import to fix linter

--- src/templates/post-template.test.js
+++ src/templates/post-template.test.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { useStaticQuery, StaticQuery } from 'gatsby';
-import PostTemplate from './post-template';
 import MockDate from 'mockdate';
+import PostTemplate from './post-template';
 import siteMetadata from '../../jest/__fixtures__/site-metadata';
 import markdownRemark from '../../jest/__fixtures__/markdown-remark';
 import type { RenderCallback } from '../types';
