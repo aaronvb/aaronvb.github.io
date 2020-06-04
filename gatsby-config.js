a80618b2a4cced611d2a16a536f67d78d9f3b9c2
Aaron Van Bokhoven <bokhoven@gmail.com>
2020-06-03 22:56:16 -1000

    Use better compressed photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -173,7 +173,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#2f3442',
         display: 'standalone',
-        icon: 'static/photo.png'
+        icon: 'static/photo.jpg'
       },
     },
     'gatsby-plugin-offline',
