9edd2e5e6bb7b899e0b10638a26129f034add5ad
Aaron Van Bokhoven <bokhoven@gmail.com>
2020-06-04 15:28:20 -1000

    Remove momentjs, use vanilla JS for date parsing

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,6 +1,5 @@
 // @flow strict
 import React from 'react';
-import moment from 'moment';
 import { Link } from 'gatsby';
 import type { Edges } from '../../types';
 import styles from './Feed.module.scss';
@@ -11,24 +10,41 @@ type Props = {
 
 const Feed = ({ edges }: Props) => (
   <div className={styles['feed']}>
-    {edges.map((edge) => (
-      <div className={styles['feed__item']} key={edge.node.fields.slug}>
-        <div className={styles['feed__item-meta']}>
-          <time className={styles['feed__item-meta-time']} dateTime={moment(edge.node.frontmatter.date).format('MMMM D, YYYY')}>
-            {moment(edge.node.frontmatter.date).format('MMM YYYY')}
-          </time>
-          <div className={styles['feed__item-inner']}>
-            <span className={styles['feed__item-title']}>
-              <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
-            </span>
-            <span className={styles['feed__item-meta-divider']} />
-            <span className={styles['feed__item-meta-category']}>
-              <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.category}</Link>
-            </span>
+    {edges.map((edge) => {
+      const parsedDate = new Date(edge.node.frontmatter.date);
+      const dateTimeFormat = new Intl.DateTimeFormat('en',
+        {
+          year: 'numeric',
+          month: 'short',
+          day: '2-digit',
+          timeZone: 'UTC'
+        });
+
+      const [
+        { value: month },,
+        { value: day },,
+        { value: year }
+      ] = dateTimeFormat.formatToParts(parsedDate);
+
+      return (
+        <div className={styles['feed__item']} key={edge.node.fields.slug}>
+          <div className={styles['feed__item-meta']}>
+            <time className={styles['feed__item-meta-time']} dateTime={`${month} ${day}, ${year}`}>
+              {month} {year}
+            </time>
+            <div className={styles['feed__item-inner']}>
+              <span className={styles['feed__item-title']}>
+                <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
+              </span>
+              <span className={styles['feed__item-meta-divider']} />
+              <span className={styles['feed__item-meta-category']}>
+                <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.category}</Link>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    ))}
+      );
+    })}
   </div>
 );
 
--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -1,16 +1,32 @@
 // @flow strict
 import React from 'react';
-import moment from 'moment';
 import styles from './Meta.module.scss';
 
 type Props = {
   date: string
 };
 
-const Meta = ({ date }: Props) => (
-  <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {moment(date).format('D MMM YYYY')}</p>
-  </div>
-);
+const Meta = ({ date }: Props) => {
+  const parsedDate = new Date(date);
+  const dateTimeFormat = new Intl.DateTimeFormat('en',
+    {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+      timeZone: 'UTC'
+    });
+
+  const [
+    { value: month },,
+    { value: day },,
+    { value: year }
+  ] = dateTimeFormat.formatToParts(parsedDate);
+
+  return (
+    <div className={styles['meta']}>
+      <p className={styles['meta__date']}>Published {day} {month} {year}</p>
+    </div>
+  );
+};
 
 export default Meta;
